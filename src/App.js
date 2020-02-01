edaa2ee2a0ef54bde138c968ea61a08f2bd30f20
Anita Kowalska <anita@MacBook-Pro-Anita.local>
2020-02-02 00:00:16 +0100

    Comparing user answer with dictionary

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import './App.css';
 
+import dict from "./dict"
 
-function App() {
-  return (
-    <div className="App">
-      <p>KOT</p>
-      <input type="text"></input>
-      <button>ZATWIERDŹ</button>
-    </div>
-  );
+function drawWord(){
+  const index = Math.floor(Math.random()*dict.length);
+  return dict[index];
+}
+
+let currentWord = drawWord()
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answer: ""
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.compareValue = this.compareValue.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({answer: event.target.value});
+  }
+
+  compareValue() {
+    this.state.answer === currentWord.JP ? 
+      console.log("Y") :
+      console.log("N")
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <p>{currentWord.PL}</p>
+        <input id="userAnswer" type="text" onChange={this.handleChange}></input>
+        <button onClick={this.compareValue}>ZATWIERDŹ</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/dict.js
@@ -0,0 +1,17 @@
+const dict = [
+    {
+        PL: "kot",
+        JP: "neko"
+    },
+    {
+        PL: "pies",
+        JP: "inu"
+    },
+    {
+        PL: "lis",
+        JP: "kitsune"
+    }
+]
+
+  export default dict;
+  
