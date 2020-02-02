9d83274bd8a0bd075ea41ec54a7ccaeb64ecfde4
Anita Kowalska <anita@MacBook-Pro-Anita.local>
2020-02-02 15:05:05 +0100

    Drawing next word

--- src/App.js
+++ src/App.js
@@ -3,44 +3,59 @@ import './App.css';
 
 import dict from "./dict"
 
-function drawWord(){
-  const index = Math.floor(Math.random()*dict.length);
-  return dict[index];
-}
-
-let currentWord = drawWord()
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.wordsList = [...dict]
     this.state = {
-      answer: ""
+      answer: "",
+      currentWord: this.drawWord()
     }
     this.handleChange = this.handleChange.bind(this);
     this.compareValue = this.compareValue.bind(this)
+  }
+
+  drawWord() {
+    const index = Math.floor(Math.random()*this.wordsList.length)
+    let [word] = this.wordsList.splice(index, 1)
+    return word
   }
 
   handleChange(event) {
     this.setState({answer: event.target.value});
   }
 
+  endList() {
+    return (this.wordsList.length === 0);
+  }
+
   compareValue() {
-    this.state.answer === currentWord.JP ? 
+    if (this.endList()) {
+      console.log("There is no more words to learn")
+    } else {
+      this.state.answer === this.state.currentWord.JP ? 
       console.log("Y") :
-      console.log("N")
+      console.log("N");
+    this.drawNext()
+    }
+  }
+
+  drawNext() {
+    this.setState({
+      answer: "",
+      currentWord: this.drawWord()
+    })
   }
 
   render () {
     return (
       <div className="App">
-        <p>{currentWord.PL}</p>
-        <input id="userAnswer" type="text" onChange={this.handleChange}></input>
+        <p>{this.state.currentWord.PL}</p>
+        <input id="userAnswer" value={this.state.answer} type="text" onChange={this.handleChange}></input>
         <button onClick={this.compareValue}>ZATWIERDŹ</button>
       </div>
     );
   }
-
 }
 
 export default App;
