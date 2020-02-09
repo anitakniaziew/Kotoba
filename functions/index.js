0ff2aa5ca1ff43c3afae7f45e6c5d554b7a346bd
Kamil Kowalski <kowalski.tychy@gmail.com>
2020-02-10 00:34:04 +0100

    Implement /phrases endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.phrases = functions.region('europe-west2').https.onRequest(async (req, res) => {
+  const snapshot = await admin.firestore().collection('phrases').get();
+  const phrases = snapshot.docs.map(doc => doc.data());
+
+  res.send({
+    data: phrases,
+    meta: {total: phrases.length}
+  });
+});
+
