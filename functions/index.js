d5b774fed5d0823412320e515dac3e476178007a
Kamil Kowalski <kowalski.tychy@gmail.com>
2020-02-20 07:17:32 +0100

    Configure Prettier for functions

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,22 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const cors = require('cors')({ origin: true });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const cors = require("cors")({ origin: true });
 
 admin.initializeApp();
 
-exports.phrasesToLearn = functions.region('europe-west2').https.onRequest(async (req, res) => {
-  return cors(req, res, async () => {
-    const snapshot = await admin.firestore().collection('phrases').get();
-    const phrases = snapshot.docs.map(doc => doc.data());
+exports.phrasesToLearn = functions
+  .region("europe-west2")
+  .https.onRequest(async (req, res) => {
+    return cors(req, res, async () => {
+      const snapshot = await admin
+        .firestore()
+        .collection("phrases")
+        .get();
+      const phrases = snapshot.docs.map(doc => doc.data());
 
-    res.send({
-      data: phrases,
-      meta: {total: phrases.length}
+      res.send({
+        data: phrases,
+        meta: { total: phrases.length }
+      });
     });
   });
-});
-
