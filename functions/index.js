0261ae8ee521d2376b4641195b7a4cc2b8f55f04
Kamil Kowalski <kowalski.tychy@gmail.com>
2020-02-22 21:55:31 +0100

    Add authorization to POST /phrases endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,24 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const cors = require("cors")({ origin: true });
+const cors = require("cors")({ origin: true, credentials: true });
 const Ajv = require("ajv");
 const schemas = require("./schemas");
 
 const https = functions.region("europe-west2").https;
 
 admin.initializeApp();
+
+const isUnauthorized = async authorizationHeader => {
+  if (!authorizationHeader) return true;
+  if (!authorizationHeader.startsWith("Bearer ")) return true;
+  const token = authorizationHeader.substring(7);
+  try {
+    await admin.auth().verifyIdToken(token);
+    return false;
+  } catch (e) {
+    return true;
+  }
+};
 
 exports.phrasesToLearn = https.onRequest(async (req, res) => {
   return cors(req, res, async () => {
@@ -25,32 +37,39 @@ exports.phrasesToLearn = https.onRequest(async (req, res) => {
 
 exports.phrases = https.onRequest(async (req, res) => {
   return cors(req, res, async () => {
-    if (req.method === "POST") {
-      const ajv = new Ajv();
-      const valid = ajv.validate(schemas["post-phrases.json"], req.body);
-
-      if (!valid) {
-        res.status(422).send({ errors: ajv.errors });
-      } else {
-        const collection = admin.firestore().collection("phrases");
-
-        await Promise.all(
-          req.body.data.map(async phrase => {
-            const snapshot = await collection
-              .where("JP", "==", phrase.JP)
-              .where("PL", "==", phrase.PL)
-              .get();
-
-            if (snapshot.empty) {
-              collection.add(phrase);
-            }
-          })
-        );
-
-        res.sendStatus(201);
-      }
-    } else {
+    if (req.method !== "POST") {
       res.sendStatus(404);
+      return;
     }
+
+    if (await isUnauthorized(req.headers.authorization)) {
+      res.sendStatus(401);
+      return;
+    }
+
+    const ajv = new Ajv();
+    const valid = ajv.validate(schemas["post-phrases.json"], req.body);
+
+    if (!valid) {
+      res.status(422).send({ errors: ajv.errors });
+      return;
+    }
+
+    const collection = admin.firestore().collection("phrases");
+
+    await Promise.all(
+      req.body.data.map(async phrase => {
+        const snapshot = await collection
+          .where("JP", "==", phrase.JP)
+          .where("PL", "==", phrase.PL)
+          .get();
+
+        if (snapshot.empty) {
+          collection.add(phrase);
+        }
+      })
+    );
+
+    res.sendStatus(201);
   });
 });
